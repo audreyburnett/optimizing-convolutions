e92dc9c3b45de574c01c07e449fb1f9017034f1e
Audrey Burnett <audrey.burnett@berkeley.edu>
2023-11-30 18:46:40 -0800

    simd

--- src/compute_optimized.c
+++ src/compute_optimized.c
@@ -31,13 +31,23 @@ int convolve(matrix_t *a_matrix, matrix_t *b_matrix, matrix_t **output_matrix) {
   //compute the covolution
   for (int r = 0; r < num_rows; r ++) {
       for (int c = 0; c < num_cols; c ++) {
-
+        __m256i sum_vec = _mm256_setzero_si256();
         uint32_t col_a = a_matrix -> cols;
         uint32_t col_b = b_matrix -> cols;
         int sum = 0;
-        for (int i = r; i < flip_matrix -> rows + r; i ++) {
-            for (int j = c; j < flip_matrix -> cols + c; j ++) {
-                sum += *(a_matrix -> data + (j) + (col_a*i))* *(flip_matrix -> data + (j-c) + (col_b*(i-r)));
+        for (int i = r; i < (flip_matrix -> rows + r); i ++) {
+            for (int j = c; j < (flip_matrix -> cols + c)/8*8; j += 8) {
+                __m256i load_a = _mm256_loadu_si256((__m256i *) *(a_matrix -> data + j + (col_a*i)));
+                __m256i load_b = _mm256_loadu_si256((__m256i *) *(flip_matrix -> data + (j-c) + (col_b*(i-r))));
+
+                __m256i product_vec = _mm256_mullo_epi32(load_a, load_b);
+                sum_vec = _mmm256_add_epi32(sum_vec, product_vec);
+            }
+            int arr[8];
+            _mm256_storeu_si256((__m256i *) arr, sum_vec);
+            sum += arr[0] + arr[1] + arr[2] + arr[3] + arr[4] + arr[5] + arr[6] + arr[7];
+            for (int k = (flip_matrix -> rows + r)/8*8; k++) {
+                sum+= a_matrix[k] * b_matrix[k];
             }
         }
         *((*output_matrix)->data + (c + r*num_cols)) = sum;
@@ -46,12 +56,6 @@ int convolve(matrix_t *a_matrix, matrix_t *b_matrix, matrix_t **output_matrix) {
   return 0;
 }
 
-int sum_simd_unrolled(matrix_t *a_matrix, matrix_t *b_matrix, matrix_t **output_matrix){
-    
-    for (unsigned int i = 0; i < len/16*16; i += 16) {
-        __m256i vec = _mm_setzero_si128();
-    }
-}
 
 // Executes a task
 int execute_task(task_t *task) {
