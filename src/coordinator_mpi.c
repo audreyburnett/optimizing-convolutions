b75d2b252a440bb3786955295acc1c48e7a25c68
Audrey Burnett <audrey.burnett@berkeley.edu>
2023-12-01 22:11:37 -0800

    task 3 openmpi

--- src/coordinator_mpi.c
+++ src/coordinator_mpi.c
@@ -14,4 +14,39 @@ int main(int argc, char *argv[]) {
   }
 
   // TODO: implement Open MPI coordinator
+  int numTasks = atoi(argv[1]);
+  MPI_Init(&argc, &argv);
+  int procID, totalProcs;
+  MPI_Comm_size(MPI_COMM_WORLD, &totalProcs);
+  MPI_Comm_rank(MPI_COMM_WORLD, &procID);
+  if (procID == 0) {
+      int nextTask=0;
+      MPI_Status status;
+      int32_t message;
+      while (nextTask < numTasks) {
+        MPI_Recv(&message, 1, MPI_INT32_T,  MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
+        int sourceProc = status.MPI_SOURCE;
+        message = nextTask;
+        MPI_Send(&message, 1, MPI_INT32_T, sourceProc, 0, MPI_COMM_WORLD);
+        nextTask++;
+      }
+      for (int i = 0; i < totalProcs - 1; i ++) {
+        MPI_Recv(&message, 1, MPI_INT32_T, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
+        int sourceProc = status.MPI_SOURCE;
+        message = TERMINATE;
+        MPI_Send(&message, 1, MPI_INT32_T, sourceProc, 0, MPI_COMM_WORLD);
+      }
+  } else {
+      int32_t message;
+      while (true) {
+        message = READY;
+        MPI_Send(&message, 1, MPI_INT32_T, 0, 0, MPI_COMM_WORLD);
+        MPI_Recv(&message, 1, MPI_INT32_T, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        if (message == TERMINATE) {
+          break;
+        }
+        execute_task(message);
+      }
+  }
+  MPI_Finalize();
 }
